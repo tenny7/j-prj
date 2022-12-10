2f3d097cd6db7f822727ad0d8cc6bb2a6ab93b28
Tennyson Onovwiona <t.onovwiona@instadeep.com>
2022-12-10 07:09:43 +0100

    First commit v1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React version 1
         </a>
       </header>
     </div>
