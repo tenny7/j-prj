b7b083577ca2cfd6edd695234d885ee0d1aee241
Tennyson Onovwiona <t.onovwiona@instadeep.com>
2022-12-11 01:29:37 +0100

    updated App.js v9

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React version 8
+          Learn React version 9
         </a>
       </header>
     </div>
