9253ce6d6bc260d5e4af682f30dfb77056db017a
Tennyson Onovwiona <t.onovwiona@instadeep.com>
2022-12-11 01:48:47 +0100

    Added Jenkinsfile

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React version 9
+          Learn React version 10
         </a>
       </header>
     </div>
